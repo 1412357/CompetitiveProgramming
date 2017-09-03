b06ca3441749163bf7584ba039bd447d795d8a03
PQNguyen <pqnguyen1996@gmail.com>
2017-09-03 17:21:32 +0700

    2 solution

---
+++ MUH and Important Things.cpp
@@ -0,0 +1,77 @@
+﻿/*
+ * Author: Phan Quang Nguyen
+ * email: pqnguyen1996@gmail.com 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int>  pii;
+
+void traveserVector(vector<pii> &a) {
+	for (pii ele : a) {
+		cout << ele.second + 1 << ' ';
+	}
+	cout << '\n';
+}
+
+int comparefunc(const void *a, const void *b) {
+	pii pa = *(pii*)a;
+	pii pb = *(pii*)b;
+	return pa.first - pb.first;
+}
+
+void input(vector<pii> &a, int &n) {
+	cin >> n;
+	a.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].first;
+		a[i].second = i;
+	}
+}
+
+bool isSatify(vector<pii> &a) {
+	int count = 0;
+	map<int, int> dub;
+	for (pii ele : a)
+		if (dub[ele.first] == 0) {
+			dub[ele.first] = 1;
+		} else {
+			count++;
+		}
+	if (count > 1)
+		return true;
+	return false;
+}
+
+void listResult(vector<pii> &a) {
+	int count = 0;
+	while (count < 3) {
+		for (int i = 0; i < a.size() - 1; i++) 
+		if (a[i].first == a[i + 1].first) {
+			swap(a[i], a[i + 1]);
+			traveserVector(a);
+			count++;
+			if (count == 3)
+				return;
+		}
+	}
+}
+
+int main() {
+	/*freopen("input.txt", "rt", stdin);*/
+	/*freopen("output.txt", "wt", stdout);*/
+	int n;
+	vector<pii> a;
+	input(a, n);
+	qsort(&a[0], a.size(), sizeof(pii), comparefunc);
+	if (isSatify(a)) {
+		cout << "YES" << endl;
+		listResult(a);
+	} else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}
---
+++ Ubiquitous Religions.cpp
@@ -0,0 +1,63 @@
+﻿/*
+ * Author: Phan Quang Nguyen
+ * email: pqnguyen1996@gmail.com 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//typedef long long ll;
+//typedef pair<int, int>  pii;
+
+int findParent(vector<int> &parent, int v) {
+	if (parent[v] != -1) {
+		parent[v] = findParent(parent, parent[v]);
+		return parent[v];
+	}
+	return v;
+}
+
+void unionSet(vector<int> &parent, int u, int v) {
+	int parentU = findParent(parent, u);
+	int parentV = findParent(parent, v);
+	if (parentU != parentV) {
+		if (parentU > parentV) {
+			parent[parentV] = parentU;
+		} else 
+			parent[parentU] = parentV;
+	}
+}
+
+int solve(int n, int m) {
+	int u, v;
+	vector<int> parent;
+	parent.resize(n, -1);
+	for (int i = 0; i < m; i++) {
+		cin >> u >> v;
+		unionSet(parent, u - 1, v - 1);
+	}
+
+	int count = 0;
+	for (int ele : parent)
+		if (ele == -1)
+			count++;
+	return count;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	/*freopen("input.txt", "rt", stdin);*/
+	/*freopen("output.txt", "wt", stdout);*/
+	int n, m;
+	int i = 1;
+	while (true) {
+		cin >> n >> m;
+		if (n == 0 && m == 0)
+			break;
+		int res = solve(n, m);
+		printf("Case %d: %d\n", i, res);
+		i++;
+	}
+	return 0;
+}
